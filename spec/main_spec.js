1b53560ea4345972cc81eb7a8ea4fdcfaf934f2c
Chiu_yan <Chiuqyan@users.noreply.github.com>
2017-12-02 16:06:25 +0800

    add

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -7,21 +7,21 @@ describe("99_bottles lyrics", function() {
         expect(res).toEqual(answer);
     });
     it("number <1", function() {
-        let answer = "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\n`";
+        let answer1 = "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\n`";
         res = main(-3456789);
         let answer = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.";
         res = main(num);
 
-        expect(res).toEqual(answer);
+        expect(res).toEqual(answer1);
     });
     it("number>99", function() {
-        let answer = "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.";
+        let answer2 = "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.";
         res = main(1234);
-        expect(res).toEqual(answer);
+        expect(res).toEqual(answer2);
     });
     it("0<number<100", function() {
-        let answer = "12 bottles of beer on the wall, 12 bottles of beer.\nTake one down and pass it around, 11 bottles of beer on the wall.";
+        let answer3 = "12 bottles of beer on the wall, 12 bottles of beer.\nTake one down and pass it around, 11 bottles of beer on the wall.";
         res = main(12);
-        expect(res).toEqual(answer);
+        expect(res).toEqual(answer3);
     });
 })
