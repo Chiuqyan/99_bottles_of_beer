b1db1b3afb49a63854028d4042b3ea719266a9e4
Chiu_yan <Chiuqyan@users.noreply.github.com>
2017-12-02 16:15:04 +0800

    add

--- main/main.js
+++ main/main.js
@@ -5,10 +5,10 @@ module.exports = function main(num) {
         countres = String(num) + " bottles of beer on the wall," + String(num) + " bottles of beer.\nTake one down and pass it around," + String(num - 1) + " bottles of beer on the wall.";
     } else if (num === 1) {
         courntres = "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\n";
-    } else if (number > 99) {
+    } else if (num > 99) {
         res = "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.";
     } else {
         res = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.";
     }
-    return res;
+    return countres;
 }
