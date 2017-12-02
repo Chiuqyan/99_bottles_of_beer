850488911478a749b9d9b9e796083965ac8223a7
Chiu_yan <Chiuqyan@users.noreply.github.com>
2017-12-02 16:34:17 +0800

    commit

--- main/main.js
+++ main/main.js
@@ -2,13 +2,13 @@ module.exports = function main(num) {
     let countres;
 
     if (num > 1 && num < 100) {
-        countres = String(num) + " bottles of beer on the wall," + String(num) + " bottles of beer.\nTake one down and pass it around," + String(num - 1) + " bottles of beer on the wall.";
+        countres = String(num) + " bottles of beer on the wall," + String(num) + " bottles of beer.\nTake one down and pass it around," + String(num - 1) + " bottles of beer on the wall.\n";
     } else if (num === 1) {
-        courntres = "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\n";
+        countres = "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\n";
     } else if (num > 99) {
-        res = "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.";
+        countres = "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n";
     } else {
-        res = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.";
+        countres = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n";
     }
     return countres;
 }
